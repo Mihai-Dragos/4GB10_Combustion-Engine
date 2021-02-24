b8271d66902a10a2b15ed5e899cdbf7dbb281b5d
JoeyJanssen01 <78683176+JoeyJanssen01@users.noreply.github.com>
2021-02-24 17:31:39 +0100

    Add files via upload

--- Other Documents/Code/4GB10_matlab_and_trainingdata/matlab/SSA4.m
+++ Other Documents/Code/4GB10_matlab_and_trainingdata/matlab/SSA4.m
@@ -1,163 +1,118 @@
 clear all;close all;clc
-
+%% add general to matlab path 
+addpath('C:\Users\20192303\Desktop\matlab\General\');
 %%
-%Importing Nasa tables
-
-addpath('C:\Users\20192303\Documents\Year 2\Quartile 3\Combustion engine\4GB10_Combustion-Engine\Other Documents\Code\4GB10_matlab_and_trainingdata\matlab\General\Nasa'); % Add directory of Nasa routines to Matlab-path
-global Runiv 
-Runiv = 8.314; %universal gas constant
-DBdir = 'General\Nasa';
-DBname = 'NasaThermalDatabase';
-load(fullfile(DBdir,DBname));
-Sp
-El
-
-%%
-%Importing data
-%for now, only example data can be used.
-
-addpath('General');
-DataDir='C:\Users\20192303\Documents\Year 2\Quartile 3\Combustion engine\4GB10_Combustion-Engine\Other Documents\Code\4GB10_matlab_and_trainingdata\Data\Training Set';ColumnOrder={'time','Sensor','Encoder'};
+DataDir='C:\Users\20192303\Desktop\matlab\gasoline';ColumnOrder={'time','Sensor','Encoder'};
 % DataDir='../Data/Gasoline';cOrder={'time'','Encoder','Sensor};
 col = lines(3);
-% Loading all measurments in DataDir
+%% Loading all measurments in DataDir
 figure(1)
-Files=dir(fullfile(DataDir,'C:\Users\20192303\Documents\Year 2\Quartile 3\Combustion engine\4GB10_Combustion-Engine\Other Documents\Code\4GB10_matlab_and_trainingdata\Data\Training Set\*.txt'));nFiles=length(Files);                  % dir gives a directory listing, only *.txt files in this case
+Files=dir(fullfile(DataDir,'*.txt'));nFiles=length(Files);                  % dir gives a directory listing, only *.txt files in this case
 for i=1:nFiles
     fname       = Files(i).name;                                            % Take a name from the list
     curfilename = fullfile(DataDir,fname);                                  % Create the full name
-    Data        = ImportData4GB10(curfilename,ColumnOrder);                             % Read the data. Type help ImportData4GB10
-    % store it for each case. Yes a struct that contains a struct and other
+    Data        = ImportData4GB10(curfilename,ColumnOrder);                 % Read the data. Type help ImportData4GB10
+    % Store it for each case. Yes a struct that contains a struct and other
     % things. Why not?
     Case(i).Data     = Data;
     Case(i).filename = fname;
     Case(i).DataDir  = DataDir;
     % preamble, put data in easy to use arrays
     t      = Data.t;
-    p      = Data.pulse;
+    Pressure      = Data.pulse;
     V      = Data.Volt;
     RevEnd = Data.RevEnds;
+    % Actual Plotting use of subplot
 end
+%% All measurements are loaded
+
+%figure(1)                                                                 %This figure plots the pulse sensor.
+%plot(t_2, V_2, 'r')
+%xlim([0.01800, 0.09800])
+%line(t(RevEnd_1-35),p_2(RevEnd_1-30),'LineStyle','none','Marker','s','MarkerSize',5,'MarkerEdgeColor','r','MarkerFaceColor','y');
+%xlabel('t [s]');
+%ylabel('pulse [V]');
+%title("Pulse sensor, 0% ethanol, full load, ")
+  
+%figure(2)                                                                 %This figure plots the pressure sensor
+%plot(t_2, V_2, 'b')
+%xlim([0, 0.3])
+%line(t(RevEnd_1),p_2(RevEnd_1),'LineStyle','none','Marker','s','MarkerSize',5,'MarkerEdgeColor','r','MarkerFaceColor','y');
+%xlabel('t [s]');ylabel('p Sensor Signal [V]');
+%title("Pressure sensor, 0% ethanol, no load, ")
 
 %%
-%Specifying dimensions
 
-%INSERT MEASUREMENTS HERE
-bore                    = 0; %[m]; diameter of the piston cylinder
-crankRadius             = 0; %[m]; radius of crankshaft
-deadVolume              = 0; %[m^3]; volume that is left when the volume in front of the piston is minimal
-connectingRodLength     = 0; %[m]; length of the rod that connects the crankshaft to the piston
-RPM                     = 0; %[revolutions per minute]; how many times the crankshaft makes a 360 degree turn in one minute
-angleCrank              = 0; %[rad]; angle the crankshaft makes, MAY BE A FUNCTION OF TIME
-%YOU'RE DONE. STOP INSERTING AFTER HERE
 
-%See slide 19 of the first presentation
-stroke                  = 2 * crankRadius; %[m]; length that the piston covers
-displacementVolume      = (pi / 4) * bore * bore * stroke; %[m^3]; volume displaced by piston
-compressionRatio        = (displacementVolume + deadVolume) / deadVolume; %[ratio]; how many times is the maximal volume larger than the smallest
-pistonVelocity          = 2 * stroke * RPM / 60; %[m/s]; how fast the piston moves up and down
-
-%See slide 20 of the first presentation
-distancePistonCrank 	= crankRadius * cos(angleCrank) + sqrt( connectingRodLength * connectingRodLength - crankRadius * crankRadius * sin(angleCrank) * sin(angleCrank) ); %[m]; distance between the crankshaft and the piston
-distancePistonCrankMax  = connectingRodLength + crankRadius; %[m]; maximum distance between the crankshaft and the piston
-distancePistonCrankMin  = connectingRodLength + crankRadius; %[m]; minimum distance between the crankshaft and the piston
-distancePistonTop       = distancePistonCrankMax - distancePistonCrank; %[m]; distance from the piston to the maximum extended position
-volume                  = pi * (bore / 2) ^ 2 * distancePistonTop + deadVolume; %[m^3]; volume compressed by the piston
+ 
 %%
-
-
-S = 0.055;
-r= 0.035;
-l= 0.088;%moet nog gemeten worden%
-rc=8.5;
+%This are some measurements from the engine.
+S=0.0190;
+rc=9.5;
+l=0.088;
+B=0.065;
 %%
+full_table = dlmread("C:\Users\20192303\Desktop\matlab\gasoline\1.txt");
+full_time = full_table(:,1);
+full_voltage = full_table(:,2);
+full_pulse = full_table(:,3);
+
+
+V_s = 5; %V, constant
+
+V_full = full_voltage * V_s;
+%2nd voltage 
+%3rd pulse
+
+%1 complete cycle is 0.04s = 1/25
+
+P_full = ((V_full/V_s-0.115)/0.0385); 
+
+
+
+
+
+%% Volume and Pv's
+%This for loop determines the volume. 
 for i=3:length(Case(1).Data.RevEnds)-2
-    
-    s_begin = RevEnd_1(i);%startpunt revolution%
-    s_end = RevEnd_1(i);
-   revs=((3000/60)*(max(Case(1).Data.t)))
-   arevs=revs*2
-   graden=arevs*360
-   cas_graden=(full_time-(s_begin/4777))*graden
-   cas_volume = (1+0.5*S - 0.5*S*cosd(cas_graden)-sqrt(l^2-(0.25*S^2*sind(cas_graden).^2))+(S/(rc-1)))*0.25*pi*0.067;
+  
+    s_begin(i) = Case(1).Data.RevEnds(i-2);
+    s_end(i) = Case(1).Data.RevEnds(i);
+    revs=((3000/60)*(max(Case(1).Data.t)))
+    arevs=revs*2;
+    graden=arevs*360;
+    Case(1).Data.cas_graden=(Case(1).Data.t-(s_begin(3)/4777))*graden
+    Case(1).Data.volume=  (l+0.5*S -0.5*S*cosd(Case(1).Data.cas_graden)- sqrt(l^2-(0.25*S^2*sind(Case(1).Data.cas_graden).^2)) +(S/(rc-1)))*0.25*pi*B^2; 
    
-   figure(9)
-   hold on
-   loglog(cas_volume(592:791), P_full(162:361))
-   grid on
-   i=i+2
+%Plotting the Pv diagrams (intervals are determined by hand)    
+    figure(7)
+    hold on
+    plot(Case(1).Data.volume(350:750), P_full(350:750))
+    i=i+2
+    
+    grid on
+title("PV-diagram Half load 10 percent")
+ylim([0, 50])
+xlabel("Volume [m^3]")
+ylabel("Pressure [Pa]")
+ 
 end
+%% Loglog
+%The Pv diagram in loglog scale
+figure()
+loglog(Case(1).Data.volume(350:750), P_full(350:750))
+ylim([0.8,23])
+grid on
+title("PV-diagram Half load 10 percent, loglog scale")
+xlabel("Volume [m^3]")
+ylabel("Pressure [Pa]")
 
+%figure(8)
+%loglog(Case(a).Data.volume(1800:2000), pres(1800:2000))
 
+%% Work 
+%The work of the Pv diagram
+Work =trapz(Case(1).Data.volume(350:750), P_full(350:750))
 
+Work_real = Work* 10^5
 
-%%
-%Thermodynamic model:
-
-%central equation:
-% 0 = Qdot - Wdot + QLHV * mdotfuel - cpexh * ( Texh - Tamb ) * mdotexh
-Qdot                = 0; %[J]; Generated heat, ONLY HOLDS WHEN ADIABATIC
-
-%%
-%Part I: Chemical bond energy
-
-%CxHyOz + (x + y / 4 - z / 2) O2 + other stuff in the air relative to the oxygen --> xCO2 + y / 2 H2O + other stuff in the air
-%ALL oxygen in the air is burnt up (stoichiometric)
-x                   = 0; %[atoms] number of CARBON atoms in a fuel molecule
-y                   = 0; %[atoms] number of HYDROGEN atoms in a fuel molecule
-z                   = 0; %[atoms] number of OXYGEN atoms in a fuel molecule
-molarmassFuel       = 0; %[g/mol] G/MOL, NOT KG/MOL!!! how heavy one mole of fuel is
-airDensity          = 101325; %[kg/m^3] density of the air at the specified temperature and elevation without fuel
-
-
-molarRatioOxygenFuel= x + y / 4 - z / 2; %[atoms] how many oxygen atoms are needed to completely combust one fuel molecule, may be a fraction.
-ratioNitrogenOxygen = 78.084 / 20.946; %[ratio] mass ratio of nitrogen to oxygen in the air 
-ratioCO2Oxygen      = 0.0407 / 20.946; %[ratio] mass ratio of nitrogen to oxygen in the air 
-%I neglect the other noble gasses present in the air
-AFstoi              = ( molarRatioOxygenFuel * 31.9988 + ratioNitrogenOxygen * molarRatioOxygenFuel * 28.0134 + ratioCO2Oxygen * molarRatioOxygenFuel * 44.01) / (molarmassFuel * 1); 
-%[ratio]; Air/fuel stoichiometric mixture determined by computing the moles
-%of each component in the air needed to burn the fuel stoiciometrically
-%multiplied by the molar mass divided by the mass of one mole of fuel.
-mair                = displacementVolume * airDensity; %[kg/cycle]
-mdotfuel            = mair / AFstoi; %[kg/cycle]; inflowing mass per second of the fuel
-
-%STILL NEEDS TO BE DONE, SEE SLIDE 43 OF FIRST PRESENTATION
-%I do not know what each variable means, or how to get that from the NASA
-%tables.
-
-QLHV                = 0; %[J/kg/cycle]; chemical energy obtainable form fuel type
-
-
-ChemicalBondEnergy  = QLHV * mdotfuel; %[J/cycle]; Energy stored in the fuel
-%alternative formula from slide 33:
-%ChemicalBondEnergy  = mfuel * cVfuel * (T3 - T2);
-%Source molar masses:
-%https://www.engineeringtoolbox.com/molecular-weight-gas-vapor-d_1156.html
-
-%%
-%Part II: Work
-%Power               = 0; %[J]; Power provided by the engine 
-
-%Wdot                = Power; %[J]; Work done by engine
-
-%%
-%Part III: Sensible heat loss
-
-%USE NASA TABLE FOR cpexh
-cpexh               = 0; %[J/K]; constant pressure heat capacity of the combustion products
-Texh                = 0; %[K]; temperature at the exit of the combustion chambre
-Tamb                = 0; %[K]; temperature of the rest of the environment (NOT NECESSARILLY FUEL INTAKE TEMPERATURE)
-
-mdotexh             = mair + mdotFuel; %[kg/cycle]; mass of the air the leaves the combustion chambre (conservation of mass)
-SensibleHeatLoss    = cpexh * ( Texh - Tamb ) * mdotexh; %[J/cycle]; heat lost during each cycle (due to passive heat dissipation and such)
-
-%%
-%Part IV: Results
-
-%rewriting the first equation
-Wdot                = ChemicalBondEnergy - Sensible; %[J/cycle]; work done by the engine each cycle
-
-bsfc                = mdotfuel / Power * 1000 / 3600000 ; %[g/kWhr]; brake specific fuel consumption
-%SPi                = mdot(OF YOUR SPECIFIC FUEL TYPE) / Power * 1000 / 3600000 %[g/kWhr];
-%specific emissions
-efficiencyCarnot    = Wdot * ChemicalBondEnergy; %[ratio]; theoretically highest obtainable efficiency
