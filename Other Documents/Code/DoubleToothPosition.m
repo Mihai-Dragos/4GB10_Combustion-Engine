ff855c7299c66e814f225ee5b1da2d92dbff70c2
VFransen <vito@sjoux.com>
2021-03-09 16:32:08 +0100

    Small error fices double tooth and test

--- Other Documents/Code/DoubleToothPosition.m
+++ Other Documents/Code/DoubleToothPosition.m
@@ -1,21 +1,15 @@
 clear all;clc
 %% add general to matlab path 
-addpath('General');
 %%
-DataDir='C:\Users\20190762\Documents\Universiteit\2020-2021 (Q3) Combustion Engine (4GB10)\4GB10_matlab_and_trainingdata\matlab\Data\No_fuel';ColumnOrder={'time','Sensor','Encoder'};
+DataDir='Data\No_fuel';ColumnOrder={'time','Sensor','Encoder'};
 % DataDir='../Data/Gasoline';cOrder={'time'','Encoder','Sensor};
 col = lines(3);
 %% Loading all measurments in DataDir
-% This script can be used to determine the position of the double tooth
-% position using max pressure of a No-fuel trial. 
-% A lot of the data collected in first two attempts is poor, and useless. 
-% Small error analysis is provided at the end
-
 Files=dir(fullfile(DataDir,'*.txt'));nFiles=length(Files);                  % dir gives a directory listing, only *.txt files in this case
 for i=1:nFiles
     fname       = Files(i).name;                                            % Take a name from the list
     curfilename = fullfile(DataDir,fname);                                  % Create the full name
-    [Data,locs, dtlocs]        = ImportData4GB10(curfilename,ColumnOrder);                             % Read the data. Type help ImportData4GB10
+    Data        = ImportData4GB10(curfilename,ColumnOrder);                             % Read the data. Type help ImportData4GB10
     % store it for each case. Yes a struct that contains a struct and other
     % things. Why not?
     Case(i).Data     = Data;
@@ -27,12 +21,14 @@ for i=1:nFiles
     V      = Data.Volt;
     RevEnd = Data.RevEnds;
     
-    if Data.NRevs >= 2;
-        [~, maxPressureID] = findpeaks(Data.Volt, 'MinPeakHeight', 0.5, 'MinPeakDistance', 3000)
+    if Data.NRevs >= 2
+        [~, maxPressureID] = findpeaks(Data.Volt, 'MinPeakHeight', 0.5, 'MinPeakDistance', 3000);
         stepAngle(i) = 360/(abs(Data.RevEnds(2) - Data.RevEnds(3)));
-        for ii = 1:2
-           stepDiff=abs(maxPressureID(2) - Data.RevEnds(ii));
-           angle(i,ii) =stepAngle(i)*stepDiff;
+        if 0 == isempty(maxPressureID)
+            for ii = 1:2
+               stepDiff=abs(maxPressureID(2) - Data.RevEnds(ii));
+               angle(i,ii) =stepAngle(i)*stepDiff;
+            end
         end
     else
         continue 
@@ -45,3 +41,74 @@ meanAngle = mean(angle);
 stdAngle = std(angle);
 
 %%
+
+function [Data]=ImportData4GB10(filename,cO)
+% [Data]=ImportData4GB10(filename,cO)
+%  WARNING: sometimes datafiles use comma as decimal separator. Than the
+%           function FAILS. We have a fast matlab tool to convert these files
+%           batchwise (see ConvertFiles.m)
+%  Input 
+%  [mandatory]  filename = full name to file to be read
+%  [optional]   cO = cell array containing column order {'time','Encoder','Sensor'} but in
+%           the order used in your data files. Above order is the default
+%  Reads datafile and stores it in a struct.
+%           Data.t          = time array
+%           Data.pulse      = pulse array
+%           Data.Volt       = sensor voltage array
+%           Data.RevEnds    = Indices to revolution end point.
+%           Data.NRevs      = Number of complete revolutions
+% Note: function trims measurement to full cycles (so even number of revs).
+%
+delim='\t';
+D=importdata(filename,delim, 0);
+%     whos D
+if (nargin < 2)
+    iT=1;iEncoder=2;iSensor=3;
+else
+    ii = myfind(cO,{'time','Encoder','Sensor'});
+    iT          =ii(1);
+    iEncoder    = ii(2);
+    iSensor     = ii(3);
+end
+t=D(:,iT);p=D(:,iEncoder);V=D(:,iSensor);
+endloc = length(t);
+% Find double tooth
+[apks,alocs]    = findpeaks(p);
+id              = find(apks > 0);
+pks             = apks(id);
+locs            = alocs(id);
+tlocs           = t(locs);
+dtlocs          = diff(tlocs);  % compute dt between maxima. Reason, double tooth has bigger dt!
+[dlocs]         = find(dtlocs > max(dtlocs)*0.5); % Pick out double tooth
+% Take full cycles (is two revolutions! this is a four-stroke engine)
+CycleIndices    = locs(dlocs);
+NRevs           = length(CycleIndices);
+Vc              = V(CycleIndices);
+if mod(NRevs,2)==0 % To make sure that only full cycles are taken.
+    fprintf('%20s\n %3i revs (will be patched\n',filename,NRevs);
+    if (Vc(1)<Vc(2))
+        fprintf('IF condition:  %9.3f < %9.3f apparently!\n',Vc(1),Vc(2));
+        CycleIndices=CycleIndices(1:end-1);
+    else
+        fprintf('ELSE condition:%9.3f > %9.3f apparently!\n',Vc(1),Vc(2));
+        CycleIndices=CycleIndices(2:end);
+    end
+else
+    fprintf('%20s\n %3i revs (will NOT be patched)\n',filename,NRevs);
+    if (Vc(1)<Vc(2))
+        fprintf('IF condition:  %9.3f < %9.3f apparently!\n',Vc(1),Vc(2));
+        CycleIndices=CycleIndices(1:end-1);
+    else
+        fprintf('ELSE condition:%9.3f > %9.3f apparently!\n',Vc(1),Vc(2));
+        CycleIndices=CycleIndices(2:end);
+    end
+end
+%     FullCycles   = [CycleIndices(1)+1:CycleIndices(end)];
+FullCycles   = [CycleIndices(1)+1:endloc];
+% Store it in the struct
+Data.t       = t(FullCycles)-t(FullCycles(1));  % Start at t=0. Shift it.
+Data.pulse   = p(FullCycles);
+Data.Volt    = V(FullCycles);
+Data.RevEnds = CycleIndices(2:end)-FullCycles(1);
+Data.NRevs   = length(Data.RevEnds);
+end
