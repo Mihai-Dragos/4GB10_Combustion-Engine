841c372a7c90defd98678000be4ec26af51f37ff
MatsvanderHeijden <m.m.s.v.d.heijden@student.tue.nl>
2021-03-17 11:16:26 +0100

    I didnt write this myself

--- Other Documents/Code/splitterPressure.m
+++ Other Documents/Code/splitterPressure.m
@@ -456,3 +456,21 @@ Data.Volt    = V(FullCycles);
 Data.RevEnds = CycleIndices(2:end)-FullCycles(1);
 Data.NRevs   = length(Data.RevEnds);
 end
+
+function [ip]=myfind(S,str)
+n=length(S);
+m=length(str);
+for j=1:m
+    i=1;
+    while ~strcmp(S(i),str(j)) & i < n
+        i=i+1;
+    end;
+    if (strcmp(S(i),str(j)))
+        %   disp(['Found ' str '=' S(i)]);
+        ip(j)=i;
+    else
+        disp([str(j) ' NOT found']);
+        ip(j)=-1;
+    end;
+end
+end
